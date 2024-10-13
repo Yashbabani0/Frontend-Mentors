e82386339aefd62bbd3612f873fbfb6d4c81ed81
Yashbabani <yashbabani09@gmail.com>
2024-10-13 15:18:21 +0530

    update path in chat app

--- chat-app-css-illustration-master/vite.config.js
+++ chat-app-css-illustration-master/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',  // Ensures correct paths for assets when deployed
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',  // Specify the output directory
+    assetsDir: 'assets',  // Specify the assets directory
+  },
+});
