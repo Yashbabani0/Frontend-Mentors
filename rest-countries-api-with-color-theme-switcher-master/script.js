aeda55cb8df9d421560904863fbd0a2cbe41d97d
Yashbabani <yashbabani09@gmail.com>
2024-10-20 05:02:11 +0530

    complete

--- rest-countries-api-with-color-theme-switcher-master/script.js
+++ rest-countries-api-with-color-theme-switcher-master/script.js
@@ -19,47 +19,101 @@ themeToggle.addEventListener("click", () => {
   }
 });
 
-fetch("https://restcountries.com/v3.1/all")
+const name = new URLSearchParams(location.search).get("name");
+const flag = document.querySelector(".flag");
+const h3 = document.querySelector("h3");
+
+const nativeName = document.querySelector(".native_name");
+const population = document.querySelector(".population");
+const region = document.querySelector(".region");
+const subRegion = document.querySelector(".sub_region");
+const capital = document.querySelector(".capital"); // Fixed typo from "captial" to "capital"
+const topLevelDomain = document.querySelector(".top_level_domain");
+const currencies = document.querySelector(".currencies");
+const languages = document.querySelector(".languages");
+
+fetch(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
   .then((res) => res.json())
   .then((data) => {
-    console.log(data); // Moved inside this .then block to correctly log the data
-    
-    data.forEach((country) => {
-      const main = document.querySelector("main");
-      const countryCard = document.createElement("a");
-      countryCard.classList.add(
-        "card",
-        "w-[16em]",
-        "shadow-md",
-        "rounded-md",
-        "h-[20em]",
-        "overflow-hidden",
-        "bg-white",
-        "dark:bg-dark-blue",
-        "transition-colors",
-        "duration-300"
-      );
+    const country = data[0];
+    console.log(country);
 
-      countryCard.innerHTML = `
-        <div class="img max-h-[10em] overflow-hidden">
-          <img
-            class="h-full w-full object-cover"
-            src="${country.flags.svg}"
-            alt="Flag of ${country.name.common}"
-          />
-        </div>
-        <div class="h-[10em] w-full flex flex-col items-start justify-center p-8 dark:text-very-light-gray">
-          <div class="name mb-2">
-            <h3 class="font-extrabold text-xl dark:text-very-light-gray">${country.name.common}</h3>
-          </div>
-          <div class="data text-very-dark-blue-text dark:text-very-light-gray text-base">
-            <p><b class="font-semibold">Population: </b><span class="font-light">${country.population.toLocaleString()}</span></p>
-            <p><b class="font-semibold">Region: </b><span class="font-light">${country.region}</span></p>
-            <p><b class="font-semibold">Capital: </b><span class="font-light">${country.capital ? country.capital[0] : "N/A"}</span></p>
-          </div>
-        </div>
-      `;
+    // Set flag and country name
+    flag.src = country.flags.svg;
+    h3.innerText = country.name.common;
 
-      main.append(countryCard);
-    });
+    // Set native name
+    if (country.name.nativeName) {
+      // Get the native names and join them into a string
+      const nativeNames = Object.values(country.name.nativeName)
+        .map((n) => n.common)
+        .join(", ");
+      nativeName.innerText = nativeNames;
+    } else {
+      nativeName.innerText = country.name.common;
+    }
+
+    // Set other details
+    population.innerText = country.population.toLocaleString(); // Format number with commas
+    region.innerText = country.region;
+    subRegion.innerText = country.subregion;
+    capital.innerText = country.capital ? country.capital[0] : "No capital";
+    topLevelDomain.innerText = country.tld ? country.tld.join(", ") : "N/A";
+
+    // Set currencies
+    if (country.currencies) {
+      const currencyNames = Object.values(country.currencies)
+        .map((c) => c.name)
+        .join(", ");
+      currencies.innerText = currencyNames;
+    } else {
+      currencies.innerText = "N/A";
+    }
+
+    // Set languages
+    if (country.languages) {
+      const languageNames = Object.values(country.languages).join(", ");
+      languages.innerText = languageNames;
+    } else {
+      languages.innerText = "N/A";
+    }
+    // Set Bordering Countries
+    const borderCountry = document.querySelector(".border_country");
+
+    if (country.borders && country.borders.length > 0) {
+      // Iterate through the borders array
+      country.borders.forEach((border) => {
+        const borderCountryButton = document.createElement("button");
+
+        // Set button properties
+        borderCountryButton.classList.add(
+          "w-[7em]",
+          "h-[3em]",
+          "text-base",
+          "font-light",
+          "shadow-md",
+          "text-very-dark-blue-text",
+          "dark:text-very-light-gray",
+          "dark:bg-dark-blue"
+        );
+
+        borderCountryButton.innerText = border;
+
+        borderCountry.appendChild(borderCountryButton);
+
+        fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+          .then((res) => res.json())
+          .then((data) => {
+            const borderCountryName = data[0].name.common;
+            borderCountryButton.innerText = borderCountryName;
+          })
+          .catch((error) => {
+            console.error(`Error fetching border country data: ${error}`);
+            borderCountryButton.innerText = "Unknown";
+          });
+      });
+    }
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
   });
